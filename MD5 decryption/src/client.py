20e3fc521abab58c4af757f4584175f93c266b89
Daniel Waitser <104393020+dking9876@users.noreply.github.com>
2022-10-07 23:26:02 +0300

    update client

--- MD5 decryption/src/client.py
+++ MD5 decryption/src/client.py
@@ -2,26 +2,17 @@ import socket
 import hashlib
 import time
 import threading
-import multiprocessing
 from multiprocessing import Process
-number_of_threads = 1
-jump = 10000000
 global number_found
 global correct_num
 global ans
-
-number_found = False
-correct_num = 0
-IP = '127.0.0.1'
-PORT = 5476
-my_socket = socket.socket()
-my_socket.connect((IP, PORT))
 global flag
-
 number_of_process = 5
+
 
 def hash(number):
     return hashlib.md5(str(number).encode("utf-8")).hexdigest()
+
 
 
 def check_num(start_num, end_num):
@@ -33,9 +24,12 @@ def check_num(start_num, end_num):
             number_found = True
             correct_num = i
 
+
 def client():
     global number_found
     global correct_num
+    global ans
+    number_of_threads = 5
     flag = True
     jump = 10000000
     number_found = False
@@ -46,11 +40,9 @@ def client():
     my_socket.connect((IP, PORT))
     msg = "send the answer"
     my_socket.send(msg.encode())
-    global ans
     ans = my_socket.recv(1024).decode()
     while flag:
         start = time.time()
-        ##time.sleep(1)
         msg = "send number"
         my_socket.send(msg.encode())
         number = my_socket.recv(1024).decode()
@@ -78,6 +70,7 @@ def client():
     my_socket.send(b"end")
     print('connection to be closed')
     my_socket.close()
+
 
 if __name__ == '__main__':
 
