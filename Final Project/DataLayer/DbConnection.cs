3f8cc7ebf64dd2d18e40e3424072dd12581f80e6
Daniel Waitser <104393020+dking9876@users.noreply.github.com>
2022-12-01 19:22:10 +0200

    create Dbconnection class

--- Final Project/DataLayer/DbConnection.cs
+++ Final Project/DataLayer/DbConnection.cs
@@ -1,15 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Azure.Cosmos;
 
 namespace DataLayer
 {
-    internal class DbConnection
+
+    public class DbConnection
     {
+        // The Cosmos client instance
+        private CosmosClient cosmosClient;
 
+        // The database we will create
+        private Database database;
 
+        // The container we will create.
+        public Container container;
 
+        // The name of the database and container we will create
+        private string databaseId = "BooksStore";
+        private string containerId = "BooksStore";
+   
+        public void Connect(string EndpointUri, string PrimaryKey)
+        {
+            // Create a new instance of the Cosmos Client
+            this.cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+            this.database = this.cosmosClient.GetDatabase(databaseId);
+            this.container = this.database.GetContainer(containerId);
+            
+        }
     }
 }
--- Final Project/Test/Program.cs
+++ Final Project/Test/Program.cs
@@ -1,17 +1,15 @@
-﻿using DataLayer;
-using System;
-
-namespace Test
+﻿namespace Test
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestDbConnection();
         }
-        static public void  TestClass1()
+        static public void  TestDbConnection()
         {
-            
+            var db = new DataLayer.DbConnection();
+            db.Connect("https://booksstore.documents.azure.com:443/", "9hRgHS70P6B2vo4KrK34Os75nFNDrp3N8eE8w6YRH0MYppNhjnUlGptXbEMKAC5EPe5ENVryQnucACDbWJyClw==");
         }
     }
 }
