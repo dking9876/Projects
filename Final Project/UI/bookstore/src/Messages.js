69a4eea0a71a692ec64389301dfbc2a76036fbeb
Daniel Waitser <104393020+dking9876@users.noreply.github.com>
2023-05-17 15:07:03 +0300

    add working messages page

--- Final Project/UI/bookstore/src/Messages.js
+++ Final Project/UI/bookstore/src/Messages.js
@@ -3,70 +3,49 @@ import { Bar } from 'react-chartjs-2';
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 
-
-async function GetMessages() {
-  const response = await fetch(`http://localhost:3000/api/message/mark/getmessages`, {
-      method: 'GET'
-  })
-  .then(response => {
-    if (response.ok) {
-      // Successful response
-      console.log('Status code:', response.status);
-      return response.json();
-    } else {
-      // Handle error response
-      console.log('Status code:', response.status);
-    }  
-  })
-  .then(data => {
-    // Process the data
-    console.log('Data:', data);
-    return data;
-  })
-  .catch(error => {
-    // Handle any errors
-    console.error('Error:', error);
-  })
-  
-}
-
 function Messages() {
-    const data = [
-        { id: 1, name: 'John', age: 25 },
-        { id: 2, name: 'Jane', age: 30 },
-        { id: 3, name: 'Bob', age: 35 },
-        { id: 4, name: 'Alice', age: 28 },
-      ];
+    const [data, setdata] = useState([])
 
-    const handleSubmit = (e) => {
+      const handleSubmit = (e) => {
         e.preventDefault();
+        fetch(`http://localhost:3000/api/message/mark/getmessages`, {
+        method: 'GET'})
+        .then(response => response.json())
+      .then(json => setdata(json))
+      .finally(() => {
         
-        // Do something with username and password, such as sending to a server
-        const response =  GetMessages()
-    }
-    //<form onSubmit={handleSubmit}>
-        //<button type="submit">Submit</button>
-      //</form>
-      return (
+      })
+      }
+
+    return (
+    <div>
+      <h1>Messages</h1>
+      <form onSubmit={handleSubmit}>
+      <button type="submit">Click to see messages</button>
+      </form>
+        <div>
         <table>
           <thead>
             <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Age</th>
+              <th>city</th>
+              <th>source</th>
+              <th>body</th>
             </tr>
           </thead>
           <tbody>
             {data.map(item => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.age}</td>
+              <tr key={item.body}>
+                <td>{item.city}</td>
+                <td>{item.source}</td>
+                <td>{item.body}</td>
               </tr>
             ))}
           </tbody>
         </table>
-      );
-    }
-  
+        </div>
+    </div>
+    )
+            }
+    
+
   export default Messages;
