249fa787096d6b1f2c105ff63d17f04b0d7a70c8
Daniel Waitser <104393020+dking9876@users.noreply.github.com>
2023-06-02 13:20:13 +0300

    add HTTPLISTENERTest

---
+++ Final Project/Samples/HttpListenerTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+using var listener = new HttpListener();
+listener.Prefixes.Add("http://localhost:7071/api/user/");
+
+listener.Start();
+
+Console.WriteLine("http://localhost:7071/api/user");
+
+while (true)
+{
+    HttpListenerContext ctx = listener.GetContext();
+    HttpListenerRequest request = ctx.Request;
+    using HttpListenerResponse resp = ctx.Response;
+    Console.WriteLine(request.RawUrl);
+
+    resp.StatusCode = (int)HttpStatusCode.OK;
+    resp.StatusDescription = "Status OK";
+}
