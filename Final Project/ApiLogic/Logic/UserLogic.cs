dea794a5b51692d93e292e5e04ff913c296b8cc4
Daniel Waitser <104393020+dking9876@users.noreply.github.com>
2023-06-05 18:17:17 +0300

    Add not finished HttpListener

---
+++ Final Project/ApiLogic/Logic/UserLogic.cs
@@ -0,0 +1,31 @@
+﻿
+using ApiLogic.Models;
+using DataLayer.DbInterfaces;
+using Newtonsoft.Json;
+
+
+
+namespace ApiLogic.Logic
+{
+    public class UserLogic
+    {
+        public static async Task<bool> Login(string body)
+        {
+           
+            var APIuser = JsonConvert.DeserializeObject<UserCreateModel>(body);
+
+            UserDB userDb = new UserDB();
+            try
+            {
+                var Checkuser = await userDb.CheckUser(APIuser.UserName, APIuser.Password, APIuser.City);
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Final Project/HttpListenerApi/Program.cs
@@ -0,0 +1,64 @@
+﻿using ApiLogic.Logic;
+using System.Net;
+namespace HttpListenerApi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HandleClientRequests().Wait();
+
+        }
+        public static async Task<IActionResult> HandleClientRequests()
+        {
+            using var listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:7071/");
+
+            listener.Start();
+
+            Console.WriteLine("http://localhost:7071/");
+
+            while (true)
+            {
+                HttpListenerContext ctx = listener.GetContext();
+                HttpListenerRequest request = ctx.Request;
+                using HttpListenerResponse resp = ctx.Response;
+
+                string HttpMethod = request.HttpMethod;
+                string RawUrl = request.RawUrl;
+                System.IO.Stream body = request.InputStream;
+                System.Text.Encoding encoding = request.ContentEncoding;
+                System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
+                string stringbody = reader.ReadToEnd();
+                if (HttpMethod == "post" && RawUrl == "user/{username}/login")
+                {
+                    bool response = await UserLogic.Login(stringbody);
+                    if (response)
+                    {
+                       
+                    }
+                    else
+                    {
+                        
+                    }
+                     
+
+                }
+                
+                body.Close();
+                reader.Close();
+
+
+                resp.StatusCode = (int)HttpStatusCode.OK;
+                resp.StatusDescription = "Status OK";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes("hello");
+                // Get a response stream and write the response to it.
+                resp.ContentLength64 = buffer.Length;
+                System.IO.Stream output = resp.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+
+            }
+        }
+    }
+}
